a6044be046bfd477c1b7c86224759b7b85e118a6
Emiliano M Alfaraz <71176986+emialfa@users.noreply.github.com>
2024-05-15 14:06:06 -0300

    Adding map in profile page

---
+++ dist/assets/button.component-06c957be.js
@@ -0,0 +1,1 @@
+import{a}from"./index-44fa87b3.js";function l({data:n}){const{label:o,onClick:r,customStyles:t,disabled:s=!1}=n;return a("button",{className:`btn ${t||"btn-primary"} lg:cursor-pointer`,onClick:r,disabled:s,children:o})}export{l as B};
